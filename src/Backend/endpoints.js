0159d2919d568df1fc0543aa7c869c0de6212415
Gabriel de Macedo Santos <gabriel.macedo@sou.inteli.edu.br>
2023-05-09 16:02:25 -0300

    add: 3 endpoints

--- src/Backend/endpoints.js
+++ src/Backend/endpoints.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors")
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 
@@ -14,23 +16,60 @@ app.use(cors())
 
 
 // Importando o banco de dados.
-const DBPATH = '.data/bd_nova_freire.db'
+const DBPATH = 'data/bd_nova_freire.db'
 
 
 // Criando endpoints
-app.post("/professor", (req, res) => {
+// POST /professor
+app.post("/professor",  urlencodedParser,(req, res) => {
     const {nome_professor, email, senha, cargo, celular, cep, idade} = req.body
 
     let db = new sqlite3.Database(DBPATH)
-
-    const query = "INSERT INTO professor(nome_professor, email, senha, cargo, celular, cep, idade) VALUES(?, ?, ?, ?, ?, ?, ?)"
-
-    db.run(query, [nome_professor, email, senha, cargo, celular, cep, idade], (error) => {
+    const query = "INSERT INTO professor(nome_professor, email, senha, cargo, celular, cep, idade) VALUES(?, ?, ?, ?, ?, ?, ?)";
+    
+    db.run(query, [nome_professor, email , senha, cargo, celular, cep, idade], (error) => {
         if (error) {
             throw new Error(error)
         }
         return res.status(201).json({
             title: "Professor criado com sucesso"
+        })
+    })
+})
+
+// PUT /professor
+app.put("/professor/:id", urlencodedParser, (req, res) => {
+    const {nome_professor, email, senha, cargo, celular, cep, idade} = req.body
+
+    // Pegando os dados do backend
+    let db = new sqlite3.Database(DBPATH)
+    const query = "UPDATE professor SET nome_professor = ?, email = ?, senha = ?, cargo = ?, celular = ?, cep = ?, idade = ? WHERE id_professor = ?"
+
+    db.run(query, [nome_professor, email, senha, cargo, celular, cep, idade, req.params.id_professor], (error) => {
+        if (error) {
+            throw new Error(error)
+        }
+        return res.status(200).json({
+            title: "Professor atualizado com sucesso."
+        })
+    })
+})
+
+
+app.get("/professor", urlencodedParser, (req, res) => {
+    
+    let db = new sqlite3.Database(DBPATH)
+
+    const query = "SELECT id_professor, * FROM professor WHERE id_professor = ?"
+
+    db.get(query, [req.params.id_professor], (error, rows) => {
+        if(error) {
+            throw new Error(error)
+        }
+
+        return res.status(200).json({
+            title: "Pofessor pegado com sucesso.",
+            professor: rows
         })
     })
 })
