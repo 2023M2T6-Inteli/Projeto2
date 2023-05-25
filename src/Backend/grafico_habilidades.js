90b3b996cef0b3f921fa8058c6a73a73b3c7a52e
eduardoferrari135 <eduardo.ferrari@sou.inteli.edu.br>
2023-05-25 17:42:25 -0300

    update: frontend

---
+++ src/Backend/grafico_habilidades.js
@@ -0,0 +1,42 @@
+const ctx2 = document.getElementById('grafico_habilidades');
+const nota2 = [10, 40, 60, 80, 90];
+const habilidades = ['H1', 'H2', 'H3', 'H4', 'H5'];
+
+new Chart(ctx2, {
+  type: 'bar',
+  data: {
+    labels: habilidades,
+    datasets: [{
+      label: 'Média por habilidade',
+      data: nota2,
+      borderWidth: 1,
+      backgroundColor: nota2.map(nota => {
+        if (nota >= 80) {
+          return 'rgb(67, 166, 51)';
+        } else if (nota >= 50) {
+          return 'rgb(242, 164, 19)';
+        } else {
+          return 'rgb(245, 83, 83)';
+        }
+      }),
+      hoverBackgroundColor: nota2.map(nota => {
+        if (nota >= 80) {
+          return 'rgb(93, 206, 76)';
+        } else if (nota >= 50) {
+          return 'rgb(255, 201, 4)';
+        } else {
+          return 'rgb(255, 103, 103)';
+        }
+      })
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
---
+++ src/Backend/grafico_turma.js
@@ -0,0 +1,43 @@
+const ctx1 = document.getElementById('grafico_turma');
+const nota1 = [12, 28, 41, 56, 62, 75, 81, 92, 97, 100];
+const datas = ["01/01", "01/02", "01/03", "01/04", "01/05", "01/06", "01/07", "01/08", "01/09", "01/10"];
+
+new Chart(ctx1, {
+  type: 'line',
+  data: {
+    labels: datas,
+    datasets: [{
+      label: 'Notas da sala',
+      data: nota1,
+      borderWidth: 1,
+      pointBackgroundColor: nota1.map(nota => {
+        if (nota >= 80) {
+          return 'rgb(67, 166, 51)';
+        } else if (nota >= 50) {
+          return 'rgb(242, 164, 19)';
+        } else {
+          return 'rgb(245, 83, 83)';
+        }
+      }),
+      pointBorderWidth: 6,
+      pointBorderColor: nota1.map(nota => {
+        if (nota >= 80) {
+          return 'rgb(67, 166, 51)';
+        } else if (nota >= 50) {
+          return 'rgb(242, 164, 19)';
+        } else {
+          return 'rgb(245, 83, 83)';
+        }
+      })
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
