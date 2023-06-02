7e5ea2fff450e2dd51157ae1282fade38aa3e351
Giovana Katsuki Murata <96273468+katsukimurata@users.noreply.github.com>
2023-06-02 09:30:41 -0300

    add: comentários

--- src/Frontend/coluna_avaliacao.js
+++ src/Frontend/coluna_avaliacao.js
@@ -1,19 +1,18 @@
 function addColumn() {
-    var table = document.getElementById("notas");
+    var table = document.getElementById("notas"); // Obtém a referência para a tabela com o id "notas"
     
-    var headerRow = table.querySelector(".questão");
-    var newHeaderCell = document.createElement("th");
-    newHeaderCell.innerHTML = '<button class="q"> Q' + (headerRow.children.length - 2) + '</button>';
-    headerRow.insertBefore(newHeaderCell, headerRow.lastElementChild);
+    var headerRow = table.querySelector(".questão"); // Obtém a linha de cabeçalho com a classe "questão"
+    var newHeaderCell = document.createElement("th"); // Cria uma nova célula de cabeçalho
+    newHeaderCell.innerHTML = '<button class="q"> Q' + (headerRow.children.length - 2) + '</button>'; // Define o conteúdo HTML da nova célula de cabeçalho
+    headerRow.insertBefore(newHeaderCell, headerRow.lastElementChild); // Insere a nova célula de cabeçalho antes da última célula
     
-    var rows = table.querySelectorAll(".valor");
+    var rows = table.querySelectorAll(".valor"); // Obtém todas as linhas com a classe "valor"
     rows.forEach(function(row) {
-      var newCell = document.createElement("td");
-      newCell.contentEditable = true;
-      row.insertBefore(newCell, row.lastElementChild);
+      var newCell = document.createElement("td"); // Cria uma nova célula
+      newCell.contentEditable = true; // Torna a célula editável
+      row.insertBefore(newCell, row.lastElementChild); // Insere a nova célula antes da última célula na linha
     });
-  }
-  
-  
+}
+
   
   
--- src/Frontend/media_avaliacao.js
+++ src/Frontend/media_avaliacao.js
@@ -1,19 +1,19 @@
 function calcularMedia(colIndex) {
-    var table = document.getElementById("notas");
-    var rows = table.querySelectorAll(".valor");
-    var sum = 0;
-    var count = 0;
+    var table = document.getElementById("notas"); // Obtém a referência para a tabela com o id "notas"
+    var rows = table.querySelectorAll(".valor"); // Obtém todas as linhas com a classe "valor"
+    var sum = 0; // Variável para armazenar a soma dos valores
+    var count = 0; // Variável para contar o número de valores
     
     rows.forEach(function(row) {
-      var cell = row.cells[colIndex];
-      var value = parseFloat(cell.textContent.trim());
+      var cell = row.cells[colIndex]; // Obtém a célula na coluna especificada pelo índice
+      var value = parseFloat(cell.textContent.trim()); // Obtém o valor da célula convertendo para um número de ponto flutuante
       
-      if (!isNaN(value)) {
-        sum += value;
-        count++;
+      if (!isNaN(value)) { // Verifica se o valor é um número válido
+        sum += value; // Adiciona o valor à soma
+        count++; // Incrementa o contador
       }
     });
     
-    var media = sum / count;
-    return media.toFixed(2); // Arredondar para 2 casas decimais
-  }
+    var media = sum / count; // Calcula a média
+    return media.toFixed(2); // Retorna a média arredondada para 2 casas decimais
+}
