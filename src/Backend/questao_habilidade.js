ca60891fac24fd6959c9737101fc9210ee7b9357
Gabriel de Macedo Santos <gabriel.macedo@sou.inteli.edu.br>
2023-06-03 00:28:38 -0300

    add: questao_habilidades.js

---
+++ src/Backend/questao_habilidade.js
@@ -0,0 +1,33 @@
+// Importando as bibliotecas que serão utilizadas.
+const express = require("express");
+const sqlite3 = require("sqlite3");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const urlcodedParser = bodyParser.urlencoded({ extended: true })
+
+// Inicializando a aplicação.
+const app = express();
+
+// Definindo regras gerais da aplicação.
+app.use(express.json())
+app.use(cors());
+
+// Importando o banco.
+const DBPATH = 'bd_nova_freire.db'
+
+// Iniciando a construção de endpoints
+// GET /questao-habilidade/:id_questao_habilidade
+app.get("/questao-habilidade/:id_questao_habilidade", urlcodedParser, (req, res) =>{
+    let db = new sqlite3.Database(DBPATH);
+    const query = "SELECT questao_habilidade.id_questao_habilidade, habilidade.id_habilidade, questao.id_questao FROM questao_habilidade JOIN habilidade ON questao_habilidade.id_habilidade = habilidade.id_habilidade JOIN questao ON questao_habilidade.id_questao = questao.id_questao WHERE id_questao_habilidade = ?";
+
+    db.all(query, [req.params.id_questao_habilidade], (error, rows) => {
+        if (error) {
+            res.json({ error: error });
+        }
+        return res.status(200).json({
+            title: "Questão habilidade pego com sucesso.",
+            data: rows
+        });
+    });
+});
