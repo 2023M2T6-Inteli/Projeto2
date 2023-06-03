685f5b3861f9f8f97ad3790b4b43222e6c9d0295
Gabriel de Macedo Santos <gabriel.macedo@sou.inteli.edu.br>
2023-06-03 00:27:34 -0300

    add: escola.js

---
+++ src/Backend/escola.js
@@ -0,0 +1,97 @@
+// Importando as bibliotecas que serão utilizadas.
+const express = require("express");
+const sqlite3 = require("sqlite3");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const urlcodedParser = bodyParser.urlencoded({ extended: true })
+
+// Inicializando a aplicação.
+const app = express();
+
+// Definindo regras gerais da aplicação.
+app.use(express.json())
+app.use(cors());
+
+// Importando o banco.
+const DBPATH = 'bd_nova_freire.db'
+
+// Iniciando a construção de endpoints
+// POST escola
+app.post("/escola", urlcodedParser, (req, res) => {
+    const {nome_escola} = req.body;
+    
+    const query = "INSERT INTO escola(nome_escola) VALUES(?)";
+    let db = new sqlite3.Database(DBPATH);
+
+    db.run(query, [nome_escola], (error) => {
+        if(error) {
+            res.status(500).json({ error: error })
+        }
+        return res.status(200).json({
+            title: "Escola criada com sucesso."
+        })
+    })
+})
+
+// PUT /escola/:id_escola
+app.put("/escola/:id_escola", urlcodedParser, (req, res) => {
+    const id_escola = req.params.id_escola;
+    const {nome_escola} = req.body;
+
+    const query = "UPDATE escola SET nome_escola = ? WHERE id_escola = ?";
+    let db = new sqlite3.Database(DBPATH)
+    db.run(query, [nome_escola, id_escola], (error) => {
+        if(error) {
+            res.status(500).json({ error: error })
+        }
+        return res.status(200).json({
+            title: "Escola alterada com sucesso."
+        })
+    })
+})
+
+
+// GET /escola/:id_escola
+app.get("/escola/:id_escola", urlcodedParser, (req, res) => {
+    const id_escola = req.params.id_escola;
+
+    const query = "SELECT rowid, * FROM escola WHERE id_escola = ?";
+    let db = new sqlite3.Database(DBPATH);
+    db.all(query, [id_escola], (error, rows) => {
+        if(error) {
+            res.status(500).json({ error: error })
+        }
+        return res.status(200).json({
+            title: "Escola encontrada.",
+            data: rows
+        })
+    })
+})
+
+// DELETE /escola/:id_escola
+app.delete("/escola/:id_escola", urlcodedParser, (req, res) => {
+    const id_escola = req.params.id_escola;
+
+    const query_selecao = "SELECT rowid, * FROM escola WHERE id_escola = ?";
+    let db = new sqlite3.Database(DBPATH);
+
+    db.all(query_selecao, [id_escola], (error, rows) => {
+        if(error) {
+            res.status(500).json({ error: error })
+        }
+        if(!rows) {
+            return res.status(404).json({
+                title: "Escola não encontrado."
+            })
+        }
+    })
+    const query = "DELETE FROM escola WHERE id_escola = ?"
+    db.run(query, [id_escola], (error) => {
+        if(error) {
+            res.status(500).json({ error: error })
+        }
+        return res.status(200).json({
+            title: "Escola deletado com sucesso."
+        })
+    })
+})
