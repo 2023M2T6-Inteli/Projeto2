c1218598c533b11a44ffd3202cffd4bc4c7ea232
eduardoferrari135 <eduardo.ferrari@sou.inteli.edu.br>
2023-06-04 17:58:36 -0300

    update: frontend sidebar

---
+++ src/Frontend/sidebar.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "230px";
+    document.getElementById("darkOverlay").style.display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("darkOverlay").style.display = "none";
+  }
