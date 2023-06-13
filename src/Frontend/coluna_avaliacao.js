cd14a90d4834f3d91afa9f3e70898ec312bb40cb
Giovana Katsuki Murata <96273468+katsukimurata@users.noreply.github.com>
2023-06-13 17:05:04 -0300

    update: colunas de avaliação

--- src/Frontend/coluna_avaliacao.js
+++ src/Frontend/coluna_avaliacao.js
@@ -1,22 +1,20 @@
 
-  
-function addColumn() {
+
+  function addColumn() {
     var table = document.getElementById("notas");
     var rows = table.getElementsByTagName("tr");
 
     // Atualiza o cabeçalho
     var headerRow = rows[0];
     var newHeaderCell = document.createElement("th");
-    newHeaderCell.innerHTML = "<button class='q'> Q" + (headerRow.childElementCount - 2) + " </button>";
-    headerRow.insertBefore(newHeaderCell, headerRow.children[headerRow.childElementCount - 2]);
+    newHeaderCell.innerHTML = "<button class='q'> Q" + (headerRow.childElementCount - 3) + " </button>";
+    headerRow.insertBefore(newHeaderCell, headerRow.children[headerRow.childElementCount - 3]);
 
     // Atualiza as células das linhas
     for (var i = 1; i < rows.length - 1; i++) {
       var valueRow = rows[i];
       var newCell = document.createElement("td");
       newCell.contentEditable = "true";
-      valueRow.insertBefore(newCell, valueRow.children[valueRow.childElementCount - 2]);
+      valueRow.insertBefore(newCell, valueRow.children[valueRow.childElementCount - 3]);
     }
   }
-
-  
