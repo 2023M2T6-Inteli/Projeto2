a2e61b0e9256d54e113ff28a05013c42d875ce76
ANNA_ARAGAO <anna.aragao@sou.inteli.edu.br>
2023-06-15 01:50:43 -0300

    Create: jwt_token_for_login
    Essa é a coisa mais bonita que já fiz nesse projeto :)

--- src/Frontend/login.js
+++ src/Frontend/login.js
@@ -1,34 +1,21 @@
 // Definindo as constantes que serão utilizas.
 // O conteúdo das constantes será os dados do formulário de login.
-const email = document.querySelector("email")
+const email = document.querySelector("#email")
 const senha = document.querySelector("#senha")
 const button = document.querySelector("#enviar")
 const url = ''
 
-// Pegando toda a URL
-let params = new URL(document.location).searchParams;
-// Puxando somente o ID da URL
-let id = params.get('email', 'senha')
-
-// Pegando dados do back com o método get()
-axios.get(`http://localhost:3000/professor/${id}`).then((res) => {
-    let login = res.data.login;
-
-    email.value = login.email
-    senha.value = login.senha
-
-}).catch((err) => {
-    throw new Error(err)    
-});
-
 button.addEventListener("click", async (event, err) => {
-    axios.post(`http://localhost:3000/professor/${id}`, {
+    axios.post(`http://localhost:3000/login`, {
         email: email.value,
         senha: senha.value
     }).then(function(res) {
-        console.log(res)
+        localStorage.setItem("token", res.data.token)
+        window.location.href = "./turma.html"
     })
     .catch(function(err){
+        alert("E-mail ou senha estão incorretos. Tente novamente.")
         throw new Error(err)
+        
     })
 })
